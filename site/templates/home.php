ca90867b4e0655e690223c1b8417a900661d2c42
L Daniel Swakman <d.swakman@gmail.com>
2016-08-29 00:00:13 +0300

    color variables and image curve

--- site/templates/home.php
+++ site/templates/home.php
@@ -12,7 +12,6 @@
   <div id="top" class="dialog-spacer"></div>
 
   <?php snippet('../templates/dialog-offer') ?>
-
   <?php snippet('../templates/dialog-contact') ?>
 
   <div onclick="closeDialog();" id="dialog_mask"></div>
@@ -21,23 +20,33 @@
 
 <main id="top">
 
-  <? // looping over subpages as sections ?>
+  <? $count = 0; // looping over subpages as sections ?>
   <? foreach ($pages->visible() as $key => $section) :?>
 
     <?
-    // determining background colour
-    switch ( $section->bg() ) {
-      case 'green' :  $bgcolor = '#9bc4b1; color: #fff'; break;
-      case 'lightestgreen' : $bgcolor = '#f3f8f3'; break;
-      case 'darkblue' : $bgcolor = '#626a82; color: #fff'; break;
-      default :  $bgcolor = '#fff';
-    }
+    $count++;
+    $bgcolor = ($section->bg()->isNotEmpty()) ? h($section->bg()) : '';
+    $textcolor = ($section->color()->isNotEmpty()) ? h($section->color()) : '';
+    $colors = array(
+      'white' => '#fff',
+      'lightestgreen' => '#f3f8f3',
+      'lightgreen' => '#b2d9c7',
+      'green' => '#9bc4b1',
+      'darkgreen' => '#8bb09f',
+      'darkblue' => '#626a82',
+      'darkgrey' => '#4a4e59',
+    );
+    if ($count > 2) {
+      $prev = $pages->visible()->nth(intval($count - 1));
+      $prevbg = ($prev->bg()->isNotEmpty()) ? h($prev->bg()) : '';
+      $prev_bgcolor = $colors[$prevbg];
+    } else { $prev_bgcolor = ''; }
     ?>
 
-    <section id="<?= $section->slug() ?>" class="section <?= $section->template() ?>" style="background-color: <?= $bgcolor ?>;">
+    <section id="<?= $section->slug() ?>" class="section <?= $section->template() ?> <?= 'bg-' . $bgcolor ?> <?= 'c-' . $textcolor ?>">
 
-      <? snippet('curve', ['color' => $bgcolor, 'key' => $section->num()]) ?>
-      <? snippet('../templates/' . $section->template(), ['section' => $section]) ?>
+      <? snippet('curve', ['color' => $colors[$bgcolor], 'key' => $section->num()]) ?>
+      <? snippet('../templates/' . $section->template(), ['section' => $section, 'prev_bgcolor' => $prev_bgcolor]) ?>
 
     </section>
 
--- site/templates/section-image.php
+++ site/templates/section-image.php
@@ -1,2 +1,11 @@
 <div id="inbetween" data-parallax="2" style="background-image: url('<?= $section->image($image)->url() ?>');">
+
+  <svg version="1.1" class="anticurve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-2499 1500.9 1400 138.2" style="enable-background:new -1799 1431.8 1400 138.2;" xml:space="preserve">
+    <style type="text/css">
+      .anticurve_path{fill: <?= $prev_bgcolor ?>;}
+    </style>
+    <path class="anticurve_path" d="M-2499,1500.9v138.2c351.8-90.2,720.5-138.2,1100.4-138.2H-2499z"/>
+    <path class="anticurve_path" d="M-1099,1500.9h-299.6c100.7,0,200.6,3.4,299.6,10V1500.9z"/>
+  </svg>
+
 </div>
