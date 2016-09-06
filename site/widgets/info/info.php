c77809499176abc42c976946576f8652a09fe1a3
L Daniel Swakman <d.swakman@gmail.com>
2016-09-06 09:59:05 +0300

    button classes

--- site/widgets/info/info.php
+++ site/widgets/info/info.php
@@ -12,7 +12,6 @@ return array(
   'html' => function() {
 
     $content = '';
-    $content .= panel()->site()->title();
     $content .= 'Adding style to a text field via <br><strong>&lt;span class="c-green"&gt;</strong> text content... <strong>&lt;/span&gt;</strong>.<br><br>';
     $content .= '<span style="background-color: #fff; display: inline-block; float: left; margin-right: 10px; width: 20px; height: 20px; border: 1px solid #eee;"></span> — c-white<br>';
     $content .= '<span style="background-color: #f3f8f3; display: inline-block; float: left; margin-right: 10px; width: 20px; height: 20px; border: 1px solid #eee;"></span> — c-lightestgreen<br>';
@@ -21,6 +20,14 @@ return array(
     $content .= '<span style="background-color: #8bb09f; display: inline-block; float: left; margin-right: 10px; width: 20px; height: 20px; border: 1px solid #eee;"></span> — c-darkgreen<br>';
     $content .= '<span style="background-color: #626a82; display: inline-block; float: left; margin-right: 10px; width: 20px; height: 20px; border: 1px solid #eee;"></span> — c-darkblue<br>';
     $content .= '<span style="background-color: #4a4e59; display: inline-block; float: left; margin-right: 10px; width: 20px; height: 20px; border: 1px solid #eee;"></span> — c-darkgrey<br>';
+
+    $content .= '<br>';
+
+    $content .= '<span style="color: #ccc;">Button styles:</span><br>';
+    $content .= '- button--primary<br>';
+    $content .= '- button--outline<br>';
+    $content .= '- button--white<br>';
+    $content .= '- button--outline-white<br>';
 
     return $content;
   }
